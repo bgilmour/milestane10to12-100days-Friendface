c926884fbcb755e3f4f110243d298178d4d9fdd1
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-25 15:23:24 +0100

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Bruce Gilmour on 2021-07-25.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
